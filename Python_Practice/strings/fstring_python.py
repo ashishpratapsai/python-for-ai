b7e5ee6c578af0a2cece9d9e5458b778895e4777
Automate With Ashish <ashishprataps.ai@gmail.com>
2026-07-26 22:37:59 +0530

    learned f string in python

---
+++ Python_Practice/strings/fstring_python.py
@@ -0,0 +1,20 @@
+
+
+name = "Ashish"
+age = 30
+language = "python"
+hours =3
+
+# using f- string
+
+print(f"{name} is {age} years old. He studies {language} {hours} hours a day.")
+
+
+sub1 = 78
+sub2 = 87
+sub3 =99
+
+
+print(f"{name} scored {sub1 + sub2 + sub3} total marks")
+
+# curly brackests willl only work if we use the f in front of the string 
