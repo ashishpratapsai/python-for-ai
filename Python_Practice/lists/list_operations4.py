0a8a7a8d9b841aab0eb23e9dd6a6b225a1f5c3e5
Automate With Ashish <ashishprataps.ai@gmail.com>
2026-08-04 23:27:55 +0530

    Mastering the numeric operations on a list, like the minimum, maximum, and sum of total numbers in a list

---
+++ Python_Practice/lists/list_operations4.py
@@ -0,0 +1,20 @@
+"""
+numneric operations on the list
+"""
+
+
+numbers  = [ 10,4,9,0,3,0,-5,-6]
+
+#min()
+#max()
+
+print(f"Smallest number : {min(numbers)}")
+print(f"Biggest number : {max(numbers)}")
+
+# remember if there are numbers and strings in ,list then we cannot compare them
+
+
+# Total of the numbers in the list 
+#sum()
+
+print(f"Total of numbers: {sum(numbers)}")
